7b0eb7407abc5f3bc9f2b223d48b3f36c01be68b
Dannielle Villanueva <6874781+Emenopi@users.noreply.github.com>
2025-05-03 14:08:17 +0100

    set up dsf layout

--- src/app/Sidebar.js
+++ src/app/Sidebar.js
@@ -6,7 +6,7 @@ export default async function Sidebar({topics}) {
       <ul className='p-20 text-black'>
         <li><Link href={'/'}>Home</Link></li>
         {
-          topics.map((topic) => (<li key={`${topic}`}>{topic}</li>))
+          topics.map((topic) => (<li key={`${topic}`} className='py-5'>{topic}</li>))
         }
       </ul>
     )
--- src/app/dsf/page.js
+++ src/app/dsf/page.js
@@ -4,18 +4,25 @@ import Sidebar from "../Sidebar"
 
 export default async function Page() {
 
-  const topicList = ['topic 1', 'topic 2', 'topic 3', 'topic 4', 'topic 5', 'topic 6', 'topic 7', 'topic 8']  
+  const topicList = [
+    'Numpy & Vectorised Compution',
+    'Visualisation',
+    'Computational Linear Algebra',
+    'Numerical Optimisation',
+    'Probability & Statistics'
+  ]  
 
    
     return (
       <>
       <Header />
       <Navbar />
-      <div className="flex flex-row p-10">
-        <div className="basis-1/8 bg-pink-200">
+      <div className="flex flex-row gap-x-10 p-10">
+        <div className="basis-1/8 bg-rose-300 rounded-md">
         <Sidebar topics={topicList} />
         </div>
-        <div className="basis-7/8"></div>
+        <div className="basis-7/8 bg-sky-950 rounded-md">
+        </div>
       </div>
       </>
     )
